325542478d2798879e5bfacb418fc43c38b7a536
Camila Pereira Murioni <camilapereiramurioni@gmail.com>
2023-11-14 20:55:05 -0300

    Js y arreglos de estructura

--- assets/Scrip/script.js
+++ assets/Scrip/script.js
@@ -8,3 +8,18 @@ function openFulImg(reference){
 function closeImg(){
     fulImgBox.style.display = "none";
 }
+
+
+function mostrarAlerta() {
+    var respuesta = prompt("Dejame tu correo para conocer lo más nuevo de mi trabajo:");
+    
+    if (respuesta !== null) {
+    alert("Gracias por tu interes, muy pronto te voy a estar enviando contenido.");
+    } else {
+    alert(" Si cambias de opinión, estaré aquí.");
+    }
+}
+
+window.onload = function() {
+    mostrarAlerta();
+};
