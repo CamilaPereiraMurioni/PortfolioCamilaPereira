4f812573f32d6e5f755c4ef4e6f8d6c72386f34b
Camila Pereira Murioni <camilapereiramurioni@gmail.com>
2023-11-15 21:16:54 -0300

    intento arreglo js

--- assets/Scrip/script.js
+++ assets/Scrip/script.js
@@ -9,22 +9,3 @@ function closeImg(){
     fulImgBox.style.display = "none";
 }
 
-    // Mensaje para que me dejen su mail posibles clientes
-    function mostrarAlertaEnInicio() {
-
-        if ((window.location.pathname === '/index.html' && !localStorage.getItem('alertaMostrada'))) {
-        var respuesta = prompt("Por favor, deja tu correo para conocer más de mi trabajo:");
-        
-        if (respuesta !== null) {
-            alert("Gracias por tu interes, muy pronto te voy a estar enviando contenido.");
-        } else {
-            alert("Si cambias de opinión, estaré aquí.");
-        }
-    
-        localStorage.setItem('alertaMostrada', 'true');
-        }
-    }
-
-    window.onload = function() {
-        mostrarAlertaEnInicio();
-    };
---
+++ assets/Scrip/scriptalert.js
@@ -0,0 +1,18 @@
+    // Mensaje para que me dejen su mail posibles clientes
+    function mostrarAlertaEnInicio() {
+        var respuesta = prompt("Por favor, deja tu correo para conocer más de mi trabajo:");
+        
+        if (respuesta !== null) {
+            alert("Gracias por tu interes, muy pronto te voy a estar enviando contenido.");
+        } 
+        
+        else {
+            alert("Si cambias de opinión, estaré aquí.");
+        }
+    
+        localStorage.setItem('alertaMostrada', 'true');
+        }
+
+    window.onload = function() {
+        mostrarAlertaEnInicio();
+    };
